38ec0e25fffaf4d609615b46474992a6701f4f90
Hung Tran <31883280+Netlabbuilder@users.noreply.github.com>
2019-09-14 23:17:43 +0200

    Create 01 or operator.py

---
+++ Python scripts/01 or operator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.6
+
+#the following examples show us how 'or' operator works
+
+# 1. output is: 3
+a = 3
+b = 5
+print(a or b)
+
+
+# 2. output is: netlabbuilder
+a = 'netlabbuilder'
+b = 5
+print(a or b)
+
+
+# 3. output is: 5
+a = ''
+b = 5
+print(a or b)
